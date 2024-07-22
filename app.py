5c45feeed70617ec91c5a6963d1d120e91eabcb3
aipper <aipper@qq.com>
2024-07-22 18:23:19 +0800

    feat: base64 read

--- app.py
+++ app.py
@@ -2,43 +2,61 @@ from flask import Flask, request, jsonify
 from PIL import Image
 import requests
 import pytesseract
+import logging
+import base64
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-def checkStr(nested_list_or_tuple):
-    results = []
-    for i in nested_list_or_tuple:
-        if isinstance(i, (list, tuple)):
-            if len(i) > 1 and isinstance(i[1], (list, tuple)) and len(i[0]) > 1:
-                if isinstance(i[1][0], str):
-                    results.append(i[1][0] + "\n")
-            results += checkStr(i)
-    return results
+def readImage(image):
+    # 使用 pytesseract 进行 OCR 识别
+    text = pytesseract.image_to_string(image, lang='chi_sim')
+    app.logger.info("text: %s", text)
 
-
-@app.route('/',methods=['POST'])
-def hello_world():
-    url = request.get_json().get('url')
-    print('url:',url)
-    image = Image.open(requests.get(url, stream=True).raw)
-    img = image.convert('L')
-    threshold = 69
-    table = []
-    for i in range(256):
-        if i < threshold:
-            table.append(1)
-        else:
-            table.append(0)
-    img = img.point(table, '1')
-    text=pytesseract.image_to_string(img, lang='chi_sim')
     data = {
         "code": 200,
-        # "result": "".join(checkStr(result))
         "result": text
     }
     return jsonify(data)
 
 
+@app.route('/file', methods=['POST'])
+def files():
+    file = request.files.get('file')
+    if file is not None:
+        return readImage(Image.open(file))
+    else:
+        return jsonify({"code": 400, "message": "file is required"}), 400
+
+
+@app.route('/base64', methods=['POST'])
+def base64():
+    base64_str = base64.b64decode(request.get_json().get('base64'))
+    image = Image.open(base64_str)
+    return readImage(image)
+
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    url = request.get_json().get('url')
+
+    # 检查 URL 是否为空
+    if not url:
+        return jsonify({"code": 400, "message": "URL is required"}), 400
+    try:
+        # 获取图像
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
+        # 打开图像
+        image = Image.open(response.raw)
+        return readImage(image)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"code": 400, "message": str(e)}), 400
+    except Exception as e:
+        return jsonify({"code": 500, "message": "An error occurred during OCR processing", "error": str(e)}), 500
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
